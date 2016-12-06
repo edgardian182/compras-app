6cdcf9ef4cb511cae25ec31a1b243455dc83750a
Edgar Quintana <edgarquintana984@hotmail.com>
2016-12-06 17:57:45 -0500

    vitrina-completa y modal sin scroll back

--- js/app.js
+++ js/app.js
@@ -1,3 +1,27 @@
+// set current position
+var cPosition = false;
+
+$('.modal').on('show.bs.modal', function(){
+
+    cPosition = $(window).scrollTop();
+})
+.on('shown.bs.modal', function(){
+
+    $('body').css({
+        position:'fixed'
+    });
+})
+.on('hide.bs.modal', function(){
+
+    $('body').css({
+        position:'relative'
+    });
+
+    window.scrollTo(0, cPosition);
+});
+
+
+
 $('.comprando').on('change','#cantidad-p',function(){
   var price = +$('#img-producto-modal').data('price');
   var cantidad = +$('#cantidad-p').val();
